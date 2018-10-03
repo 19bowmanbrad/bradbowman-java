7e7c0d08503e86b345db7c73ce78449c378209b5
RDL2020 <Lenr@bellarmineprep.org>
2018-10-03 10:10:14 -0700

    rocketShip draft two from Bellarmine.

--- rocketShip/src/rocketShip.java
+++ rocketShip/src/rocketShip.java
@@ -30,19 +30,73 @@ public class rocketShip {
     }
 
     public static void bottomMidSection() {
-        for (int i = 0; i < 2; i++) {
-            for (int j = 6; j > i; j--) {
+        for (int i = 0; i < 3; i--) {
+            for (int j = 2; j > i; j++) {
+                System.out.print(".");
+            }
+            for (int k = -1; k < i; k--) {
+                System.out.print("/\\");
+            }
+            for (int j = 2; j > i; j++) {
+                System.out.print(".");
+            }
+            for (int j = 2; j > i; j++) {
+                System.out.print(".");
+            }
+            for (int k = -1; k < i; k--) {
+                System.out.print("/\\");
+            }
+            for (int j = 2; j > i; j++) {
+                System.out.print(".");
+            }
+            System.out.println();
+
+
+
+
+
+
+
+
+
+
+
+
+            /*for (int j = 6; j > i; j--) {
                 System.out.print("\\/");
             }
             System.out.println();
-            for (int k = -1; k < i; k++) {
+            for (int k = 1; k > i; k--){
                 System.out.print(".");
             }
-            for (int j = 0; j > i; j--) {
+            for (int j = 2; j > i; j--){
                 System.out.print("\\/");
             }
-
-
+            for (int k = 2; k > i; k--) {
+                System.out.print(".");
+            }
+            for (int j = 2; j > i; j--){
+                System.out.print("\\/");
+            }
+            for (int k = 1; k > i; k--) {
+                System.out.print(".");
+            }
+            System.out.println();
+            for (int k = 2; k > i; k--) {
+                System.out.print(".");
+            }
+            for (int j = 1; j > i; j--){
+                System.out.print("\\/");
+            }
+            for (int k = 4; k > i; k--) {
+                System.out.print(".");
+            }
+            for (int j = 1; j > i; j--){
+                System.out.print("\\/");
+            }
+            for (int k = 2; k > i; k--) {
+                System.out.print(".");
+            }*/
         }
     }
 }
