8affe2e5774d86dc8e329d92dfafaeb5374dd49a
RDL2020 <Lenr@bellarmineprep.org>
2018-10-09 09:19:38 -0700

    rocketShip draft four from Bellarmine.

--- rocketShip/src/rocketShip.java
+++ rocketShip/src/rocketShip.java
@@ -1,13 +1,30 @@
 public class rocketShip {
+    public static final int SIZE = 5;
+
     public static void main(String[] args) {
         topSectionHalfOne();
+        connecter();
         topMidSection();
         bottomMidSection();
+        connecter();
+        bottomMidSection();
+        topMidSection();
+        connecter();
+        topSectionHalfOne();
     }
-
+    public static void connecter(){
+        for(int i = 0; i < 1; i++){
+            System.out.print("+");
+            for (int j = 0; j < 6; j++){
+                System.out.print("=*");
+            }
+            System.out.println("+");
+        }
+    }
 
     public static void topMidSection() {
         for (int i = 0; i < 3; i++) {
+            System.out.print("|");
             for (int j = 2; j > i; j--) {
                 System.out.print(".");
             }
@@ -26,12 +43,13 @@ public class rocketShip {
             for (int j = 2; j > i; j--) {
                 System.out.print(".");
             }
-            System.out.println();
+            System.out.println("|");
         }
     }
 
     public static void bottomMidSection() {
         for (int i = 0; i < 3; i++) {
+            System.out.print("|");
             for (int k = 0; k < i; k++) {
                 System.out.print(".");
             }
@@ -50,25 +68,21 @@ public class rocketShip {
             for (int k = 0; k < i; k++) {
                 System.out.print(".");
             }
-            System.out.println();
+            System.out.println("|");
         }
     }
     public static void topSectionHalfOne(){
-        for (int i = 0; i < 5; i++){
-            for (int k = i; k < 4; k++){
+        for (int i = 0; i < SIZE; i++){
+            for (int k = i; k < SIZE; k++){
                 System.out.print(" ");
             }
-            for (int j = -1; j < i; j++){
+            for (int j = 0; j <= i; j++){
                 System.out.print("/");
             }
             System.out.print("**");
-            for (int j = -1; j < i; j++){
+            for (int j = 0; j <= i; j++){
                 System.out.print("\\");
             }
-            for (int k = i; k < 4; k++){
-                System.out.print(" ");
-            }
-
             System.out.println();
         }
     }
