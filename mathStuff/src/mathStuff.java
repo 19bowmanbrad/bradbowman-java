120a5f060a28c35ea01cf64db1a8d6132f1c1a62
RDL2020 <Lenr@bellarmineprep.org>
2018-10-22 08:57:20 -0700

    mathStuff(return value lab) final from Bellarmine.

--- mathStuff/src/mathStuff.java
+++ mathStuff/src/mathStuff.java
@@ -1,10 +1,12 @@
 public class mathStuff {
     public static void main(String[] args) {
         int answer = addNum( 3, 4);
-        System.out.println(answer);
+        System.out.println("The sum of these numbers is " + answer);
         int minimum = minNum(4,8,12);
-        System.out.println(minimum);
-    }
+        System.out.println("The minimum is " + minimum);
+        double minPlusMax = minMaxSum(3,6,13);
+        System.out.println("The square root of the sum of the minimum and maximum is " +minPlusMax);
+     }
 
     public static int addNum(int num1, int num2) {
         int sum = (num1 + num2);
@@ -13,4 +15,10 @@ public class mathStuff {
     public static int minNum (int num1, int num2, int num3){
         return Math.min((Math.min(num1, num2)), num3);
     }
+    public static double minMaxSum (double num1, double num2, double num3){
+        double minimum = Math.min((Math.min(num1, num2)), num3);
+        double maximum = Math.max((Math.min(num1, num2)), num3);
+        return (Math.sqrt(minimum + maximum));
+    }
 }
+
