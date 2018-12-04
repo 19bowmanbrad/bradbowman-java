627d94461da1502491f4a2ab72f9f32a8e340f2e
19bowmanbrad <19bowmanbradley@bprep.org>
2018-12-04 09:20:39 -0800

    4.1 draft two.

--- workSheetFourOne/src/workSheetFourOne.java
+++ workSheetFourOne/src/workSheetFourOne.java
@@ -34,5 +34,37 @@ public class workSheetFourOne {
         //3a
         System.out.println(arrayFive[(arrayFive.length -1)]);
         //3b
+        int[] middle = makeMiddle(arrayFive);
+        System.out.println(Arrays.toString(middle));
+        //3c
+        for (int i = 0; i < arrayFive.length; i++){
+            arrayFive[i]--;
+        }
+        System.out.println(Arrays.toString(arrayFive));
+        //3d
+        int count = 0;
+        int[] even = new int [500];
+        for (int x = 0; x < 1000; x+=2){
+            even[count] = x;
+            count ++;
+        }
+        System.out.println(Arrays.toString(even));
+        //3f
+        System.out.println((even[100]));
+        //3g
+        System.out.println((even[278]) + " " + even[456]);
+    }
+    //3b
+    public static int[] makeMiddle(int[] nums){
+        int[] a;
+        if (nums.length % 2 == 0){
+            a = new int[2];
+            a[0] = nums[(nums.length/2) - 1];
+            a[1] = nums[nums.length/2];
+        } else{
+            a = new int[1];
+            a[0] = nums[nums.length/2];
+        }
+        return a;
     }
 }
