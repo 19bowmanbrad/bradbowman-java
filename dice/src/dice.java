b887d26df1a561cf58f5fcc9f13ea809b9dbb470
19bowmanbrad <19bowmanbradley@bprep.org>
2018-12-13 08:38:00 -0800

    Dice draft three from Bellarmine.

--- dice/src/dice.java
+++ dice/src/dice.java
@@ -2,26 +2,23 @@ import java.util.*;
 public class dice {
     public static final int DICENUM = 1;
     public static void main(String[] args){
-        String dice[][] = new String [6][5];
+        String gameDice[][] = new String [6][5];
         Scanner userInput = new Scanner(System.in);
         System.out.println("Would you like to play a game? -y -n");
         String yesNo = userInput.next();
         int roll = rollDice();
+        gameDice = defDice(roll, gameDice);
+
         while(yesNo.equals("y")){
-            String gameDice[][] = new String [DICENUM][6];
-            gameDice = defDice(roll, dice);
-//            System.out.println(Arrays.toString(gameDice));
-//            System.out.println(gameDice.length);
-            roll = rollDice();
+            gameDice = defDice(roll, gameDice);
             printDice(gameDice);
-            rollDice();
+            roll = rollDice();
             drawLine();
             System.out.println("Keep playing? -y -n");
             yesNo = userInput.next();
         }
     }
     public static String[][] defDice(int roll, String [][] dice) {
-        System.out.println("defdice");
         String topBottom =  " ------- ";
         String blank =      "|       |";
         String oneStar =    "|   *   |";
@@ -51,29 +48,28 @@ public class dice {
             dice[i][1] = twoStar;
             dice[i][2] = oneStar;
             dice[i][3] = twoStar;
+        } else if (roll == 5){
+            dice[i][1] = twoStar;
+            dice[i][2] = twoStar;
+            dice[i][3] = twoStar;
         }
         return dice;
 
     }
     public static void printDice(String [][] dice) {
-        System.out.println("printDice");
         for (int i = 0; i < dice.length - 1; i++){
             System.out.println(dice[DICENUM - 1][i]);
         }
-
-//        for (int i = 0; i < dice.length; i++){
-//            System.out.println(dice[i][i]);
-//        }
     }
     public static int rollDice(){
-        System.out.println("rollDice");
         Random rand = new Random();
         int roll = rand.nextInt(6);
-        System.out.println(roll);
         return(roll);
     }
     public static void drawLine(){
-        System.out.println("drawLine");
+        for (int i = 0; i < 20; i ++){
+            System.out.print("_");
+        }
     }
 
 }
