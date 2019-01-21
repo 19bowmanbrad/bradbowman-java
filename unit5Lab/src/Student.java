0ea576f91d4034264f4f8741878a9241ecf4773f
19bowmanbrad <19bowmanbradley@bprep.org>
2019-01-21 12:25:10 -0800

    unit5Lab FINAL from home.

--- unit5Lab/src/Student.java
+++ unit5Lab/src/Student.java
@@ -1,35 +1,50 @@
 import java.util.*;
-import java.util.ArrayList;
 public class Student {
+
 
     String studentName;
     String studentGrade;
+    ArrayList<Double> grades;
 
-    private void getName(String newName){
+
+    public void getName(String newName){
         studentName = newName;
     }
-    private void getGrade(String newGrade){
+    public void getGrade(String newGrade){
         studentGrade = newGrade;
     }
+    public void newGrades(ArrayList<Double> newGrades){
+        grades = newGrades;
+    }
 
-    ArrayList<Double> grades = new ArrayList<>();
-    grades.add(98.5)
 
-    private boolean passingOrFailing(ArrayList<Double> myGrades){
+    public boolean passingOrFailing(ArrayList<Double> myGrades){
         double sum = 0;
         double grade;
         for (int i = 0; i < myGrades.size(); i ++){
             sum += myGrades.get(i);
         }
         double average = (sum/myGrades.size());
-        if (average > 65){
+        if (average > 65.0){
             return true;
         } else {
             return false;
         }
     }
-    private int numClasses(ArrayList<Double> myGrades){
-
+    public int numClasses(){
+        return grades.size();
+    }
+    public String studentName(){
+        return studentName;
+    }
+    public String yearGrade(){
+        return studentGrade;
+    }
+    public void addGrades(Double newGrade){
+        grades.add(newGrade);
+    }
+    public String toString(){
+        return studentName + " is a " + studentGrade;
     }
 
 
